c7a6205c552f7b65f07ae88d56cd9d82d0f9cbd9
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-08-30 22:30:40 -0300

    Add voyager to libraries. Add Second and Third screens

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -2,29 +2,52 @@ package org.basic.project
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.PathNode
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 
 @Composable
 fun App() {
     MaterialTheme {
+        Navigator(
+            screen = MainScreen()
+        )
+    }
+}
+
+class MainScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
         var name: String by remember { mutableStateOf("") }
         var password: String by remember { mutableStateOf("") }
 
-        Column (
+        Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
@@ -35,7 +58,10 @@ fun App() {
                 value = name,
                 onValueChange = {
                     name = it
-                }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
             )
 
             TextField(
@@ -45,7 +71,9 @@ fun App() {
                 }
             )
 
-            Spacer(modifier = Modifier.height(40.dp))
+            Spacer(
+                modifier = Modifier
+                    .height(40.dp))
 
             AnimatedVisibility(name.isNotEmpty()) {
                 Text(
@@ -56,8 +84,118 @@ fun App() {
 
             AnimatedVisibility(name.isNotEmpty()) {
                 Text(
-                    text = "Versión animada $password",
+                    text = "Contraseña: $password",
                     fontSize = 18.sp
+                )
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp))
+
+            Button(
+                onClick = {
+                    navigator.push(SecondScreen())
+                }
+            ) {
+                Text(
+                    text = "Navegando"
+                )
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp))
+
+            Button(
+                onClick = {
+                    navigator.push(ThirdScreen())
+                }
+            ) {
+                Text(
+                    text = "Plus Ultra"
+                )
+            }
+        }
+    }
+}
+
+class SecondScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+            text = "Segunda pantalla",
+            fontSize = 22.sp,
+            color = Color.Black
+        )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Vamos para atrás"
+                )
+            }
+        }
+    }
+}
+
+class ThirdScreen: Screen {
+
+    @Composable
+    override fun Content() {
+
+        val navigator: Navigator = LocalNavigator.currentOrThrow
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Cyan),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Non Plus Ultra",
+                fontSize = 22.sp,
+                color = Color.Black
+            )
+
+            Text(
+                text = "Hasta acá llega esta tarea.",
+                fontSize = 22.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Volvamos"
                 )
             }
         }
