6917bf8876b7ff53da9a5cf9a79b588b46968014
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-08-30 21:27:49 -0300

    Add second TextField + AnimatedVisibility

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -1,20 +1,29 @@
 package org.basic.project
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf("") }
+        var password: String by remember { mutableStateOf("") }
+
         Column (
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -22,15 +31,35 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "¡Hola mundo!"
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                }
             )
-            Text(
-                text = "Programación 2"
+
+            TextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                }
             )
-            Text(
-                text = "Android Studio"
-            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(name.isNotEmpty()) {
+                Text(
+                    text = "Versión animada $name",
+                    fontSize = 24.sp
+                )
+            }
+
+            AnimatedVisibility(name.isNotEmpty()) {
+                Text(
+                    text = "Versión animada $password",
+                    fontSize = 18.sp
+                )
+            }
         }
     }
 }
